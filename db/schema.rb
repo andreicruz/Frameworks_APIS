c3c0126cff04d51ea77403660169940582eb1781
andreicruz <andrei_cruz97@hotmail.com>
2019-05-08 22:07:08 -0300

    db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_20_190716) do
+ActiveRecord::Schema.define(version: 2019_05_08_230305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,15 @@ ActiveRecord::Schema.define(version: 2019_04_20_190716) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "produtos", force: :cascade do |t|
+    t.string "nome"
+    t.string "descricao"
+    t.float "preco"
+    t.bigint "categoria_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["categoria_id"], name: "index_produtos_on_categoria_id"
+  end
+
+  add_foreign_key "produtos", "categorias"
 end
