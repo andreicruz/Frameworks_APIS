1463c2a4d279b94e7a53cd9b00c355030990921f
andreicruz <andrei_cruz97@hotmail.com>
2019-05-09 22:58:20 -0300

    adicionado estilo no follower

--- app/assets/javascripts/categorias.js
+++ app/assets/javascripts/categorias.js
@@ -2,8 +2,10 @@ $(window).scroll(function() {
     var scroll = $(window).scrollTop();
     if (scroll >= 1) {
         $(".header-web").addClass("follower");
+        $(".follower").css("background-color", "rgba(0,0,0,0.5)");
     }else{
         $(".header-web").removeClass("follower");
+        $(".header-web").css("background-color", "#11110e");
     }
 });
 
