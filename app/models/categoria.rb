7831980d2862668e4ed6f5b3a2c4395593328b18
andreicruz <andrei_cruz97@hotmail.com>
2019-05-10 22:41:50 -0300

    adicionada dependencia restrict_with_error

--- app/models/categoria.rb
+++ app/models/categoria.rb
@@ -1,5 +1,5 @@
 class Categoria < ApplicationRecord
-    has_many :produtos
+    has_many :produtos, dependent: :restrict_with_error
     validates :nome, presence: true,
                     length: { minimum: 3 }
 end
