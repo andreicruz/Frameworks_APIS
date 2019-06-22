c66bcc751ec4b59bddae2da401695506b2624684
Andrei Cruz <andrei_cruz@terceiros.sicredi.com.br>
2019-06-22 15:55:39 -0300

    adicionado render json

--- app/controllers/categorias_controller.rb
+++ app/controllers/categorias_controller.rb
@@ -1,10 +1,14 @@
 class CategoriasController < ApplicationController
+    skip_before_action :verify_authenticity_token
     def index
         @categorias = Categoria.all.order("created_at ASC")
+        # API REST
+        render json: {status: 'SUCCESS', message:'Categorias carregadas', data:@categorias},status: :ok
     end
     
     def show
         @categoria = Categoria.find(params[:id])
+        render json: {status: 'SUCCESS', message:'Categoria', data:@categoria},status: :ok
     end
     
     def new
@@ -20,8 +24,10 @@ class CategoriasController < ApplicationController
        
         if @categoria.save
           redirect_to @categoria
+          render json: {status: 'SUCCESS', message:'Categoria Salva', data:@categoria},status: :ok
         else
           render 'new'
+          render json: {status: 'ERROR', message:'Erro ao Criar', data:@categoria.erros},status: :unprocessable_entity
         end
     end
        
@@ -30,8 +36,10 @@ class CategoriasController < ApplicationController
        
         if @categoria.update(categoria_params)
           redirect_to @categoria
+          render json: {status: 'SUCCESS', message:'Categoria editada', data:@categoria},status: :ok          
         else
           render 'edit'
+          render json: {status: 'ERROR', message:'Erro ao editar', data:@categoria.erros},status: :unprocessable_entity
         end
     end
 
@@ -39,8 +47,10 @@ class CategoriasController < ApplicationController
         @categoria = Categoria.find(params[:id])
         if @categoria.destroy
             flash[:success] = 'Categoria deletada'
+            render json: {status: 'SUCCESS', message:'Categoria Deletada', data:@categoria},status: :ok
         else
             flash[:danger] = 'Categoria vinculada a um produto, impossível deletar'
+            render json: {status: 'ERROR', message:'Erro ao Deletar', data:@categoria.erros},status: :unprocessable_entity
         end
      
         redirect_to categorias_path
